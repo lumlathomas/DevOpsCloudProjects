aed7942fc594102c9e12f0507d37c58b328287cf
Lumla Thomas <lumlarose@gmail.com>
2025-03-22 21:02:57 +0000

    Update config.js

--- 3-tier-app-with-CICD-k8-Docker-IaC-AWS/client/src/pages/config.js
+++ 3-tier-app-with-CICD-k8-Docker-IaC-AWS/client/src/pages/config.js
@@ -1,5 +1,5 @@
 // // const API_BASE_URL = "http://localhost:8800";
-const API_BASE_URL = "http://api.lumlathomas.site";
+const API_BASE_URL = "http://api.lumlathomas.site"; // change it accordinly
 // export default API_BASE_URL;
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
 // export default API_BASE_URL;
